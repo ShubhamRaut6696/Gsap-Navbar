3a1aa425b517d4ea2f8446f6dc1b9df5f7e9ee19
Shubham Raut <shubhamraut47271@gmail.com>
2024-06-23 14:50:51 +0530

    gsap navbar

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var menu = document.querySelector(".outernavbar i")
+var close = document.querySelector(".innernavbar i")
+
+var tl = gsap.timeline()
+
+tl.to(".innernavbar",{
+    right:0,
+    duration:0.5,
+
+})
+
+tl.from(".innernavbar h4",{
+    x:100,
+    duration:0.5,
+    stagger:0.2,
+    opacity:0
+})
+
+tl.pause()
+menu.addEventListener("click", function(){
+    tl.play()
+})
+close.addEventListener("click", function(){
+    tl.reverse()
+})
